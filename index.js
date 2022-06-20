b9246ae3a97447de27afcaba8441a714dd435dcc
Mariana <marianaolivera83@gmail.com>
2022-06-20 20:48:07 -0300

    Agregando index

---
+++ index.js
@@ -0,0 +1,31 @@
+/*const BLANCO =" "
+let palabraUno
+palabraUno="Hola"
+let palabraDos="mundo"
+
+
+let palabraTres= palabraUno+BLANCO+ palabraDos
+
+console.log(palabraTres)
+ const NUMEROUNO = 2
+ let numeroDos= 5
+ let suma=NUMEROUNO+numeroDos
+ let division=NUMEROUNO / numeroDos
+ let multi = NUMEROUNO * numeroDos
+ console.log(suma)
+ console.log(division)
+ console.log(multi)
+ let numeroCuatro=5
+ 
+ console.log(numeroCuatro)
+ let nombreIngresado = prompt("Ingrese nombre")
+ console.log(nombreIngresado)
+
+ let entrada =prompt("Ingresar una letra")
+ let salida = entrada+ " "+"ingresada"
+ alert(salida) */
+ //solicitar dos numeros al usuario y sumar//
+ let numeroUno=prompt("Ingrese el primer numero")
+ let numeroDos=prompt("Ingrese el segundo numero")
+ let suma= parseFloat(numeroUno) + parseFloat(numeroDos)
+ alert("la suma es :"+suma)
